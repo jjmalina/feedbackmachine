ceaa585c453ae50da0c88dbcb3b559edb2e14104
Jeremiah Malina <jmalina327@gmail.com>
2013-02-07 23:34:18 -0500

    Resources

--- app/api.py
+++ app/api.py
@@ -1,3 +1,4 @@
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from app.models import Event, Demo, Comment
 
@@ -5,16 +6,20 @@ from app.models import Event, Demo, Comment
 class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
+        authorization = Authorization()
         resource_name = 'events'
-
+        allowed_methods = ['get']
 
 class DemoResource(ModelResource):
     class Meta:
         queryset = Demo.objects.all()
+        authorization = Authorization()
         resource_name = 'demos'
+        allowed_methods = ['get']
 
 
 class CommentResource(ModelResource):
     class Meta:
         queryset = Comment.objects.all()
+        authorization = Authorization()
         resource_name = 'comments'
