8e324c1048bf1001e7383781cda8afc6262bf121
Jeremiah Malina <jmalina327@gmail.com>
2013-02-09 16:41:49 -0500

    Cleaned up code a little bit

--- app/views.py
+++ app/views.py
@@ -3,25 +3,24 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from models import *
+from app.models import Event, Demo, Comment
 
 import json
 
 
 def current_demo(request, event_id):
     event = Event.objects.get(id=event_id)
-    current_demo = {
+    current_demo_json = json.dumps({
         'demo_id': event.current_demo.id
-    }
-    current_demo_json = json.dumps(current_demo)
+    })
     return HttpResponse(current_demo_json)
 
 
 @csrf_exempt
 @require_POST
 def create_comment(request, demo_id):
-    comment = Comment(demo_id=demo_id, content=request.POST.get('content'))
-    comment.save()
+    comment = Comment.objects.create(
+        demo_id=demo_id, content=request.POST.get('content'))
     comment_json = json.dumps({
         'demo_id': comment.demo_id,
         'created_at': str(comment.created_at),
